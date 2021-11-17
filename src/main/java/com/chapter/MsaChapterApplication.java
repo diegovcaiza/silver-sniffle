41bde81b71aac9e32f68c7b6e29eb0ece09aada3
dvcaiza <dvcaiza@pichincha.com>
2021-11-16 19:09:21 -0500

    Config project

---
+++ src/main/java/com/chapter/MsaChapterApplication.java
@@ -0,0 +1,13 @@
+package com.chapter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsaChapterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsaChapterApplication.class, args);
+	}
+
+}
