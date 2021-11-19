7c54fc9ed41af1245e2475af2c965589fe3f8839
CARLA FERNANDEZ <cefernan@pichincha.com>
2021-11-19 12:57:48 -0500

    Clase Jugador

---
+++ src/main/java/com/chapter/model/Players.java
@@ -0,0 +1,15 @@
+package com.chapter.model;
+
+
+import lombok.Data;
+
+/**
+ * @author cefernan
+ *
+ */
+@Data
+public class Players {
+
+    private String jugador1;
+    private String jugador2;
+}
